95c4e51dbf1af63a72296282aae7e2019aa9c9a1
seongminnpark <seongmi1@andrew.cmu.edu>
2016-12-09 18:48:17 -0500

    Locate inject position

--- content-script.js
+++ content-script.js
@@ -1,1 +1,16 @@
 // content-script.js
+
+var frames = document.getElementsByName ("if_comment_yesorno");
+var commentFrame = frames[0];
+var commentParagraph = commentFrame.parentNode
+var bottomInstance = commentParagraph.parentNode
+var postInstance = bottomInstance.parentNode
+
+var elemDiv = document.createElement('div');
+elemDiv.style.cssText = 'width:400px;height:200px;background-color:blue;';
+
+postInstance.appendChild(elemDiv);
+
+
+
+
