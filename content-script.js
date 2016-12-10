f1c482a25ec8fe6a6306af535814b1164e13bf45
seongminnpark <seongmi1@andrew.cmu.edu>
2016-12-09 19:19:19 -0500

    Add page number div

--- content-script.js
+++ content-script.js
@@ -1,16 +1,27 @@
 // content-script.js
 
-var frames = document.getElementsByName ("if_comment_yesorno");
-var commentFrame = frames[0];
-var commentParagraph = commentFrame.parentNode
-var bottomInstance = commentParagraph.parentNode
-var postInstance = bottomInstance.parentNode
+var inject = function() {
+    var frames = document.getElementsByName ("if_comment_yesorno");
+    var commentFrame = frames[0];
+    var commentParagraph = commentFrame.parentNode
+    var bottomInstance = commentParagraph.parentNode
+    var postInstance = bottomInstance.parentNode
 
-var elemDiv = document.createElement('div');
-elemDiv.style.cssText = 'width:400px;height:200px;background-color:blue;';
+    var postList = document.createElement('div');
+    var pageNumber = getParamsMap()["page"];
+    postList.innerHTML = '<li>Test 테스트.. Page Number: ' + pageNumber + ' </li>';
+    postInstance.appendChild(postList);
+}
 
-postInstance.appendChild(elemDiv);
+var getParamsMap = function () {
+    var uri = window.location.href;
+    var params = uri.substring(uri.indexOf('?')+1).split('&');
+    var paramsMap = {};
+    params.forEach(function (p) {
+        var v = p.split("=");
+        paramsMap[v[0]]=decodeURIComponent(v[1]);
+    });
+    return paramsMap;
+};
 
-
-
-
+inject();
